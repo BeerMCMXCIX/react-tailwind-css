744420adbc2b687704dd84d4a98e46dc65e567b2
BeerMCMXCIX <noppolphonart@gmail.com>
2025-09-05 07:40:03 +0700

    add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
 import './App.css'
 import Navbar from './components/Navbar'
+import Header from './components/Header'
+import Clients from './components/Clients'
+import Community from './components/Community'
+import Pixelgrade from './components/Pixelgrade'
+import Stats from './components/Stats'
+import How from './components/How'
+import Meet from './components/Meet'
+import Demo from './components/Demo'
+import Footer from './components/Footer'
 
 function App() {
 
   return (
     <>
       < Navbar />
+      < Header />
+      < Clients />
+      < Community />
+      < Pixelgrade />
+      < Stats />
+      < How />
+      < Meet />
+      < Demo />
+      < Footer />
     </>
   )
 }
